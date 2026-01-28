b9cacede43ac664ed3d1740ae05bbd255a053a36
Yeman1801 <janghelayush777@gmail.com>
2026-01-28 13:38:49 +0530

    Add files via upload

--- To_do Task1.py
+++ To_do Task1.py
@@ -19,7 +19,7 @@ def taskadd():
         return messagebox.showwarning("Missing data", "All fields is required")
     d = time(s, e)
     if not d:
-        return messagebox.showerror("Time error", "24 hours Invalid time")
+        return messagebox.showerror("Time error", "Invalid time (24 hours time)")
     tt2.insert("", tk.END, values=(t, s, e, d))
     e1.delete(0, tk.END)
     e2.delete(0, tk.END)
@@ -54,38 +54,37 @@ def colon(event, box):
     box.delete(0, tk.END)
     box.insert(0, v)
 
-
+button = {"font": ("bold", 13), "bd": 4, "relief": "solid"}
 
 t1 = tk.Label(tk1, text="To-Do-list with Time traker", bg="#d8ebfd", font=("Segoe UI", 20, "bold"))
 t1.pack()
 
 l1 = tk.Label(tk1, text="Task-Name", font=("Arial", 14, "bold"))
 l1.place(x=75, y=60)
-
 l2 = tk.Label(tk1, text="Start-Time", font=("Arial", 14, "bold"))
 l2.place(x=320, y=60)
-
 l3 = tk.Label(tk1, text="End-Time", font=("Arial", 14, "bold"))
 l3.place(x=560, y=60)
 
+enter = {"width" : 30, "bd": 4, "relief": "solid"}
 
-e1 = tk.Entry(tk1, width=30)
+e1 = tk.Entry(tk1, **enter)
 e1.place(x=40, y=100, height=30)
 
-e2 = tk.Entry(tk1, width=30)
+e2 = tk.Entry(tk1, **enter)
 e2.place(x=280, y=100, height=30)
 e2.bind("<KeyRelease>", lambda e: colon(e, e2))
 
-e3 = tk.Entry(tk1, width=30)
+e3 = tk.Entry(tk1, **enter)
 e3.place(x=520, y=100, height=30)
 e3.bind("<KeyRelease>", lambda e: colon(e, e3))
 
 
-b1 = tk.Button(tk1, text="Add Task", command=taskadd, font=("Arial", 12, "bold"))
-b1.place(x=200, y=150)
+b1 = tk.Button(tk1, text="Add Task", command=taskadd, **button)
+b1.place(x=200, y=140)
 
-b2 = tk.Button(tk1, text="Delete Task", command=taskdelete, font=("Arial", 12, "bold"))
-b2.place(x=400, y=150)
+b2 = tk.Button(tk1, text="Delete Task", command=taskdelete, **button)
+b2.place(x=400, y=140)
 
 
 cols = ("Task", "Start", "End", "Duration")
